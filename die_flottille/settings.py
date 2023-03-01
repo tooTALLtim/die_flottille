4382b30a2988ee803a63b9a0f890ea74933ea813
Tim Davis <mitted2@gmail.com>
2023-02-28 18:46:41 -0700

    installed django debug toolbar

--- die_flottille/settings.py
+++ die_flottille/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
+    'flottille',
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
---
+++ flottille/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FlottilleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'flottille'
