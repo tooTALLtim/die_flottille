89e43ba0ab5fa0261523fa9347416bfdb2528cfc
Tim Davis <mitted2@gmail.com>
2023-03-01 21:22:56 -0700

    created models: ship, captain, XO

--- flottille/models.py
+++ flottille/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Ship(models.Model):
+    name = models.CharField(max_length=50)
+    nickname = models.CharField(max_length=50, blank=True)
+
+
+class Captain(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=100, default='no nicknames found')
+    origin = models.CharField(max_length=50, default='unknown origin')
+    description = models.TextField()
+
+
+class XO(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    nickname = models.CharField(max_length=100, default='no nicknames found')
+    origin = models.CharField(max_length=50, default='unknown origin')
+    description = models.TextField()
+
+
