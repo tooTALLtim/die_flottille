898d7112d352ff9b123e643abd18d28930ab1b3b
Tim Davis <mitted2@gmail.com>
2023-03-03 00:41:05 -0700

    made SpaceShip serializer and view

---
+++ flottille/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import *
+
+class SpaceShipSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = SpaceShip
+        fields = ['name', 'nickname', 'classification', 'armament', 'engine_type']
--- flottille/views.py
+++ flottille/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.viewsets import ModelViewSet
+from .models import *
+from .serializers import *
+
 
 def hello(request):
     return render(request, 'hello.html')
+
+
+class SpaceShipViewSet(ModelViewSet):
+    queryset = SpaceShip.objects.all()
+    serializer_class = SpaceShipSerializer
