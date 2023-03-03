d4dd3077de37dfdaab6dc3d2e70167a2f8374bd3
Tim Davis <mitted2@gmail.com>
2023-03-03 00:24:19 -0700

    installed django rest framework

--- die_flottille/urls.py
+++ die_flottille/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('flottille/', include('flottille.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
